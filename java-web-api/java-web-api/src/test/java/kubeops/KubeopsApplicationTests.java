50d9de7ce531514a837d907ce51102caf11aeada
tunsarut <59427850+tunsarut@users.noreply.github.com>
2021-03-26 18:32:43 +0700

    Add files via upload

---
+++ java-web-api/java-web-api/src/test/java/kubeops/KubeopsApplicationTests.java
@@ -0,0 +1,13 @@
+package kubeops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
